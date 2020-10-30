27123a971ed4a03b3c8b1daa026655596fdc4a23
HemesAlsaria <73545418+HemesAlsaria@users.noreply.github.com>
2020-10-30 22:05:03 +0000

    x is z and square of 24

--- src/my_square.py
+++ src/my_square.py
@@ -1,10 +1,10 @@
-def my_square(x):
+def my_square(z):
 <<<<<<< HEAD
 =======
    """takes a value and returns the squared value 
    uses the ** operator 
    """
 >>>>>>> b57e25afa9ee1edecf6d9d4b12281ce3774e3bab
-    return(x ** 2)
+    return(z * z)
    
-print(my_square(4))
+print(my_square(24))
